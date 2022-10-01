4b47460fe13965ad3b27629f549ec4ecb4a55409
NinjadenMu <jaden.mu@gmail.com>
2022-10-01 16:14:45 -0400

    update

--- gerrymander.py
+++ gerrymander.py
@@ -1,2 +1,158 @@
 from region import *
+import random
+import math
+import time
 
+region = Region((20, 20), False, [(5, 5, 1), (17, 14, 1), (6, 12, -1), (16, 6, -1)], [(7, 8, 10000), (3, 7, 8000), (16, 18, 8000), (5, 16, 8000), (14, 3, 8000)])
+region.create_tiles()
+region.assign_tile_populations()
+region.assign_tile_voter_preferences()
+
+class Gerrymander:
+    def __init__(self, region, party):
+        self.region = region
+        self.party = party
+
+    def voronoi_districts(self, num_districts):
+        district_centers = []
+
+        for district in range(num_districts):
+            while 1:
+                tile = random.choice(random.choice(self.region.tiles))
+
+                if tile.district == -1:
+                    tile.district = district
+                    district_centers.append(tile.position)
+                    break
+
+        for row in self.region.tiles:
+            for tile in row:
+                distances = []
+                if tile.district == -1:
+                    for district_center in district_centers:
+                        distances.append(((district_center[0] - tile.position[0]) ** 2 + (district_center[1] - tile.position[1]) ** 2) ** 0.5)
+
+                    tile.district = distances.index(min(distances))
+            
+        districts = [[] for district in range(num_districts)]
+        for row in self.region.tiles:
+            for tile in row:
+                districts[tile.district].append(tile)
+
+        self.districts = districts
+
+        #self.region.display_region()
+        #for district in districts:
+            #self.region.display_region(True, district)
+
+
+    def score_district(self, num_districts, first_eval = True, pop_deltas = []):
+        if first_eval:
+            self.populations = [0 for district in range(num_districts)]
+
+            for row in self.region.tiles:
+                for tile in row:
+                    self.populations[tile.district] += tile.population
+
+        else:
+            for i, delta in enumerate(pop_deltas):
+                self.populations[i] += delta
+
+        total_deviation = 0
+        average = sum(self.populations) / num_districts
+        for population in self.populations:
+            total_deviation += (population - average) ** 2
+
+        std_dev = math.sqrt(total_deviation)
+
+        if first_eval:
+            pass
+
+        else:
+            for i, delta in enumerate(pop_deltas):
+                self.populations[i] -= delta
+
+        return std_dev
+
+    def get_edge_tiles(self,first_pass = True):
+        if first_pass:        
+            edge_tiles = []
+            for row in self.region.tiles:
+                for tile in row:
+                    edge_tile_data = []
+                    if (tile.position[0] + 1) < region.dimensions[0] and (tile.position[1] + 1) < region.dimensions[1] and self.region.tiles[tile.position[0] + 1][tile.position[1] + 1].district != tile.district:
+                        edge_tile_data.append(self.region.tiles[tile.position[0] + 1][tile.position[1] + 1].district)
+
+                    if (tile.position[0] - 1) >= 0 and (tile.position[1] - 1) >= 0 and self.region.tiles[tile.position[0] - 1][tile.position[1] - 1].district != tile.district:
+                        edge_tile_data.append(self.region.tiles[tile.position[0] - 1][tile.position[1] - 1].district)
+
+                    if (tile.position[0] - 1) >= 0 and (tile.position[1] + 1) < region.dimensions[1] and self.region.tiles[tile.position[0] - 1][tile.position[1] + 1].district != tile.district:
+                        edge_tile_data.append(self.region.tiles[tile.position[0] - 1][tile.position[1] + 1].district)
+
+                    if (tile.position[0] + 1) < region.dimensions[0] and (tile.position[1] - 1) >= 0 and self.region.tiles[tile.position[0] + 1][tile.position[1] - 1].district != tile.district:
+                        edge_tile_data.append(self.region.tiles[tile.position[0] + 1][tile.position[1] - 1].district)
+
+                    if edge_tile_data != []:
+                        edge_tile_data.insert(0, tile)
+                        edge_tiles.append(edge_tile_data)
+
+        #lse:
+
+        return edge_tiles
+
+    def dfs(self, visited, node_on, district):
+        if len(visited) == len(district) - 1:
+            return True
+
+        edges = [(1, 1), (1, -1), (-1, 1), (-1, -1)]
+
+        for edge in edges:
+            
+
+    def check_if_continuous(self, district):
+
+
+    def mcmc(self, num_districts):
+        total_time = 0
+        self.voronoi_districts(num_districts)
+        print(self.score_district(num_districts))
+
+        for i in range(1000):
+            original_score = self.score_district(num_districts)
+            start = time.perf_counter()
+            tile_to_flip = random.choice(self.get_edge_tiles(True))
+            stop = time.perf_counter()
+            total_time += stop - start
+            original_district = tile_to_flip[0].district
+
+            tile_to_flip[0].district = random.choice(tile_to_flip[1:])
+            self.districts[original_district].remove(tile_to_flip[0])
+            self.districts[tile_to_flip[0].district].append(tile_to_flip[0])
+
+            new_score = self.score_district(num_districts)
+
+            if original_score < new_score and random.random() > 0.4:
+                self.districts[original_district].append(tile_to_flip[0])
+                self.districts[tile_to_flip[0].district].remove(tile_to_flip[0])
+                tile_to_flip[0].district = original_district
+        
+        print(self.score_district(num_districts))
+        print(total_time)
+        districts = [[] for district in range(num_districts)]
+        for row in self.region.tiles:
+            for tile in row:
+                districts[tile.district].append(tile)
+
+        self.region.display_region()
+        for district in districts:
+            self.region.display_region(True, district)
+    
+
+
+
+
+        
+
+
+gerrymander = Gerrymander(region, 'red')
+gerrymander.mcmc(5)
--- region.py
+++ region.py
@@ -7,23 +7,17 @@ import matplotlib.pyplot as plt
 
 
 class Tile:
-    def __init__(self, position, population = 0, voter_preference = 0, instability = 0):
+    def __init__(self, position, population = 0, voter_preference = 0, instability = 0, district = -1):
         self.position = position #tuple, 1st element is row, 2nd is column
 
         self.population = population #population of tile
         self.voter_preference = voter_preference #partisanship of territory from -1 to 1
         self.instability = instability #likelyhood of tile being moved to new district from 0 to 1
 
-class District:
-    def __init__(self, tiles, population = 0, voter_preference = 0):
-        self.tiles = tiles #tiles in district
-
-        self.population = population #total population of district
-        self.voter_preference = voter_preference #combined partisanship of territory from -1 to 1
-
+        self.district = district
 
 class Region:
-    def __init__(self, dimensions, random_distribution = True, vote_source_points = [], population_source_points = [], max_population_variation = 0.02):
+    def __init__(self, dimensions, random_distribution = True, vote_source_points = [], population_source_points = []):
         self.dimensions = dimensions #tuple, 1st element is rows, 2nd is columns
 
         self.random_distribution = random_distribution #boolean for if population and partisanship distribution are randomly generated or from source points
@@ -35,8 +29,6 @@ class Region:
             self.vote_source_points = vote_source_points #source points of maximum partisanship from which voters spread out
             self.population_source_points = population_source_points #source points of densest population from which population spreads out
 
-        self.max_population_variation = max_population_variation #maximum population variation between districts
-
         self.tiles = [[] for row in range(dimensions[0])] #tiles in region
         self.districts = [[] for col in range(dimensions[1])] #districts in region
 
@@ -46,32 +38,40 @@ class Region:
                 self.tiles[row].append(Tile((row, column)))
 
     def simulate_agent_movements(self, agent_starting_pos, agents):
+        #print(self.dimensions[0], self.dimensions[1])
         for agent in agents: #simulate movement for every agent
             agent_pos = [agent_starting_pos[0], agent_starting_pos[1]]
+            #print('starting pos')
+            #print(agent_pos)
             #print(agent_pos)
             for move in range(agent): #make moves until agent has made maximum number of moves
                 while True: #make random moves to adjacent squares, retry if move puts agent in invalid location
                     move = random.choice([(-1, 0), (0, 1), (1, 0), (0, -1)])
                     agent_pos[0] += move[0]
                     agent_pos[1] += move[1]
-
-                    if (0 <= agent_pos[0] < self.dimensions[0]) and (0 <= agent_pos[1] < self.dimensions[1]): #break if agent location is valid
+                    #print('og')
+                    #print(agent_pos)
+                    if (0 <= agent_pos[0] and agent_pos[0] < self.dimensions[0]) and (0 <= agent_pos[1] and agent_pos[1] < self.dimensions[1]): #break if agent location is valid
+                        #print('filtered')
+                        #print(agent_pos[0], agent_pos[1])
                         break
 
                     else: #undo previous move if agent move is not valid
                         agent_pos[0] -= move[0]
                         agent_pos[1] -= move[1]
-
+            #print('final pos')
+            #print(agent_pos)
             self.tiles[agent_pos[0]][agent_pos[1]].population += 1 #once agent location is finalized add to tile population
 
 
     def assign_tile_populations(self):                
         if self.random_distribution:
-            agents = [] #each agent contributes 1 to the population
-            for agent in range(self.population): #randomly assign each agent a number of moves based on a distribution centered on the amount of moves it takes to reach a corner
-                agents.append(int(np.random.uniform(0, self.dimensions[0] + self.dimensions[1])))
+            for i in range(3): #create 3 population centers
+                agents = [] #each agent contributes 1 to the population
+                for agent in range(self.population // 3): #randomly assign each agent a number of moves based on a distribution centered on the amount of moves it takes to reach a corner
+                    agents.append(int(np.random.uniform(0, self.dimensions[0] + self.dimensions[1])))
 
-            self.simulate_agent_movements((self.dimensions[0] // 2, self.dimensions[1] // 2), agents) #move each agent to final location
+                self.simulate_agent_movements((random.randint(0, self.dimensions[0] - 1),random.randint(0, self.dimensions[1] - 1)), agents) #move each agent to final location
 
         else:
             for source_point in self.population_source_points: #for each source pointcreate agent and do distribution
@@ -107,22 +107,79 @@ class Region:
         for row in self.tiles:
             for tile in row:
                 tile.voter_preference = self.calculate_voter_preference(tile.position, self.vote_source_points) #get each tile's preference from source point
-                
+
+    def return_voter_split(self):
+        blue_total = 0
+        red_total = 0
+        for tile_row in self.tiles:
+            for tile in tile_row:
+                if tile.voter_preference < 0:
+                    blue_total -= tile.voter_preference * tile.population
+                    
+                else:
+                    red_total += tile.voter_preference * tile.population
+
+        return blue_total * (100 / (blue_total + red_total)), red_total * (100 / (blue_total + red_total))
+
+    def display_region(self, filter = False, filter_list = []):
+        #create 3d bar graph representing region
+        fig = plt.figure()
+        ax1 = fig.add_subplot(111, projection = '3d')
+
+
+        x_pos = np.arange(0, self.dimensions[0], 1)
+        y_pos = np.arange(0, self.dimensions[1], 1)
+        x_pos, y_pos = np.meshgrid(x_pos, y_pos)
+        x_pos = x_pos.flatten()
+        y_pos = y_pos.flatten()
+        z_pos = [0] * self.dimensions[0] * self.dimensions[1]
+
+        dxy = [1] * self.dimensions[0] * self.dimensions[1] #xy size of each bar
+        dz = [] #z height of each bar
+        for row in self.tiles:
+            for tile in row:
+                if tile in filter_list or not filter:
+                    dz.append(tile.population)
+
+                else:
+                    dz.append(0)
+
+        colors = []
+        for row in self.tiles: #assign each bar color based on voter preference
+            for tile in row:
+                if tile in filter_list or not filter:
+                    if tile.voter_preference < 0:
+                        colors.append(((1+tile.voter_preference)/1.1, (1+tile.voter_preference)/1.1, 1, 1))
+
+                    else:
+                        colors.append((1, (1-tile.voter_preference)/1.16, (1-tile.voter_preference)/1.16, 1))
+
+                else:
+                    colors.append((1, 1, 1))
+
+        ax1.bar3d(x_pos, y_pos, z_pos, dxy, dxy, dz, color = colors) 
+        plt.show() #display graph 
             
 
 
 if __name__ == '__main__':
-    region = Region((20, 20), True) #initialize randomized 20x20 region
+    region = Region((20, 20), False, [(5, 5, 1), (17, 14, 1), (6, 12, -1), (16, 6, -1)], [(7, 8, 10000), (3, 7, 8000), (16, 18, 8000), (5, 16, 8000), (14, 3, 8000)]) #initialize randomized 20x20 region
     region.create_tiles() #initialize region tiles
     region.assign_tile_populations() #generate populations
     region.assign_tile_voter_preferences() #generate voter preferences
-
+    print(region.return_voter_split())
+    region.display_region()
+    """
     #create 3d bar graph representing region
     fig = plt.figure()
     ax1 = fig.add_subplot(111, projection = '3d')
 
-    x_pos = [i for i in range(region.dimensions[0]) for j in range(region.dimensions[0])] #xyz of each bar
-    y_pos = [i for i in range(region.dimensions[1])] * region.dimensions[1]
+
+    x_pos = np.arange(0, region.dimensions[0], 1)
+    y_pos = np.arange(0, region.dimensions[1], 1)
+    x_pos, y_pos = np.meshgrid(x_pos, y_pos)
+    x_pos = x_pos.flatten()
+    y_pos = y_pos.flatten()
     z_pos = [0] * region.dimensions[0] * region.dimensions[1]
 
     dxy = [1] * region.dimensions[0] * region.dimensions[1] #xy size of each bar
@@ -138,6 +195,7 @@ if __name__ == '__main__':
                 colors.append((1, (1-tile.voter_preference)/1.16, (1-tile.voter_preference)/1.16, 1))
 
     ax1.bar3d(x_pos, y_pos, z_pos, dxy, dxy, dz, color = colors) 
-    plt.show() #display graph
+    plt.show() #display graph 
+    """
 
 
